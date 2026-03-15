69e7cd69ad9a215c0a70d71812106f777b1897d1
venkatyashwanth <mudiliyashwanth@gmail.com>
2026-03-15 11:53:22 +0530

    modified about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,11 +4,10 @@ export default function About() {
         <section className={styles.about}>
             <h2 className={styles.title}>About Me</h2>
             <p className={styles.description}>
-                Frontend Developer with 4 years of experience working in enterprise
-                environments such as NW18 (MoneyControl) and Sony Pictures Network.
-                My core strength lies in building clean UI, interactive components,
-                and scalable web applications,while collaborating with React teams Most of my advanced React
-                experience comes from production-grade personal projects.
+                Frontend Developer with 4 years of experience working in enterprise environments such as NW18 (MoneyControl) and Sony Pictures Network. I specialize in building clean, responsive user interfaces, interactive components and scalable web applications.
+            </p>
+            <p className={styles.description}>
+                I enjoy translating designs into high-quality UI and collaborating with engineering teams to deliver reliable production features. I also build advanced personal projects using React and Next.js to continuously improve my frontend architecture skills.
             </p>
         </section>
     )
